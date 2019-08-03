5eca7786b9404313cdd665afb5e94cc501d30796
MessaoudAzidane <fm_azidane@esi.dz>
2019-08-03 21:07:14 +0100

    adding UserDetails

---
+++ src/main/java/com/mesut/springsecurity/UserPrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package com.mesut.springsecurity;
+
+import com.mesut.springsecurity.db.UserRepository;
+import com.mesut.springsecurity.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepository userRepository ;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        User user = this.userRepository.findByUsername(s);
+        UserPrincipal userPrincipal = new UserPrincipal(user) ;
+
+        return userPrincipal;
+    }
+}
