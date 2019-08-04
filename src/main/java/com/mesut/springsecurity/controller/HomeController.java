1c16a4cd849418876259b2b5aed88115bcf7af0d
MessaoudAzidane <fm_azidane@esi.dz>
2019-08-04 18:52:52 +0100

    Adding login page

--- src/main/java/com/mesut/springsecurity/controller/HomeController.java
+++ src/main/java/com/mesut/springsecurity/controller/HomeController.java
@@ -2,11 +2,22 @@ package com.mesut.springsecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+
+    @GetMapping("/login")
+    public String loging(){
+        return "login" ;
+    }
+
+    //@PostMapping("")
+
+
     @GetMapping("index")
     public String index(){
         return "index";
